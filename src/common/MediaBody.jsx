9554932d470028e340e418eeccaa5c1ea691107d
Ige olasoji <olasojiige@gmail.com>
2019-09-02 20:04:41 +0100

    client

--- src/common/MediaBody.jsx
+++ src/common/MediaBody.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
 const MediaBody = ({ complain }) => {
+  const DATE_OPTIONS = {
+    weekday: 'short',
+    minutes: '',
+    hour: 'numeric',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+  };
+  const date = new Date(complain.date).toLocaleDateString(
+    'en-US',
+    DATE_OPTIONS
+  );
+
+  console.log(date);
+
   return (
     <div className='col-md-10 col-12 '>
       <div className='media-body'>
         <h5 className='mt-md-0 mt-2 mb-1'>
-          {complain.title} |<small> {complain.complain_category}</small>
+          {complain.title} |
+          <small>
+            {' '}
+            {complain.complain_category} <i className='fa fa-clock-o'></i>{' '}
+            {date}
+          </small>
         </h5>
 
         {complain.body}
